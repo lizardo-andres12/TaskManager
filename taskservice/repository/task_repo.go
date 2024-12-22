1766d59a50ba35e112a65aae57a789780e5f8978
lizardo-andres12 <lizardo.andres12@gmail.com>
2024-12-22 18:34:52 -0500

    add: Completed worker repo impl

--- taskservice/repository/task_repo.go
+++ taskservice/repository/task_repo.go
@@ -2,13 +2,11 @@ package repository
 
 import (
 	"database/sql"
-	"sync"
 	"taskservice/models"
 )
 
 type TaskRepo struct {
 	db *sql.DB
-	mu sync.Mutex
 }
 
 func NewTaskRepo(db *sql.DB) *TaskRepo {
@@ -16,9 +14,6 @@ func NewTaskRepo(db *sql.DB) *TaskRepo {
 }
 
 func (taskrepo *TaskRepo) CreateNew(record *models.Task) error {
-	taskrepo.mu.Lock()
-	defer taskrepo.mu.Unlock()
-
 	_, err := taskrepo.db.Exec("INSERT INTO tasks (Title, Priority, UserID, CreatedAt) VALUES (?, ?, ?, ?)", record.Title, record.Priority, record.UserID, record.CreatedAt)
 	if err != nil {
 		return err
@@ -27,8 +22,6 @@ func (taskrepo *TaskRepo) CreateNew(record *models.Task) error {
 }
 
 func (taskrepo *TaskRepo) GetByID(ids ...uint64) (*models.Task, error) {
-	taskrepo.mu.Lock()
-	defer taskrepo.mu.Unlock()
 	var task models.Task
 
 	row := taskrepo.db.QueryRow("SELECT * FROM tasks WHERE id = ?", ids[0])
@@ -39,8 +32,6 @@ func (taskrepo *TaskRepo) GetByID(ids ...uint64) (*models.Task, error) {
 }
 
 func (taskrepo *TaskRepo) GetAll(limit int, id uint64) ([]models.Task, error) {
-	taskrepo.mu.Lock()
-	defer taskrepo.mu.Unlock()
 	var tasks []models.Task
 
 	rows, err := taskrepo.db.Query("SELECT * FROM tasks WHERE userId = ? LIMIT ?", id, limit)
@@ -63,9 +54,6 @@ func (taskrepo *TaskRepo) GetAll(limit int, id uint64) ([]models.Task, error) {
 }
 
 func (taskrepo *TaskRepo) UpdateExisting(id uint64, record *models.Task) error {
-	taskrepo.mu.Lock()
-	defer taskrepo.mu.Unlock()
-
 	_, err := taskrepo.db.Exec("UPDATE tasks SET title = ?, priority = ? WHERE id = ?", record.Title, record.Priority, id)
 	if err != nil {
 		return err
@@ -74,9 +62,6 @@ func (taskrepo *TaskRepo) UpdateExisting(id uint64, record *models.Task) error {
 }
 
 func (taskrepo *TaskRepo) DeleteByID(id uint64) error {
-	taskrepo.mu.Lock()
-	defer taskrepo.mu.Unlock()
-
 	_, err := taskrepo.db.Exec("DELETE FROM tasks WHERE id = ?", id)
 	if err != nil {
 		return err
--- taskservice/repository/worker_repo.go
+++ taskservice/repository/worker_repo.go
@@ -2,13 +2,11 @@ package repository
 
 import (
 	"database/sql"
-	"sync"
 	"taskservice/models"
 )
 
 type WorkerRepo struct {
 	db *sql.DB
-	mu sync.Mutex
 }
 
 func NewWorkerRepo(db *sql.DB) *WorkerRepo {
@@ -16,9 +14,6 @@ func NewWorkerRepo(db *sql.DB) *WorkerRepo {
 }
 
 func (workerrepo *WorkerRepo) CreateNew(record *models.Worker) error {
-	workerrepo.mu.Lock()
-	defer workerrepo.mu.Unlock()
-
 	_, err := workerrepo.db.Exec("INSERT INTO workers VALUES (userId, taskId, username)", record.UserID, record.TaskID, record.Username)
 	if err != nil {
 		return err
@@ -27,8 +22,6 @@ func (workerrepo *WorkerRepo) CreateNew(record *models.Worker) error {
 }
 
 func (workerrepo *WorkerRepo) GetByID(ids ...uint64) (*models.Worker, error) {
-	workerrepo.mu.Lock()
-	defer workerrepo.mu.Unlock()
 	var worker models.Worker
 
 	row := workerrepo.db.QueryRow("SELECT * FROM workers WHERE userId = ? AND taskId = ?", ids[0], ids[1])
@@ -39,8 +32,6 @@ func (workerrepo *WorkerRepo) GetByID(ids ...uint64) (*models.Worker, error) {
 }
 
 func (workerrepo *WorkerRepo) GetAll(limit int, id uint64) ([]models.Worker, error) {
-	workerrepo.mu.Lock()
-	defer workerrepo.mu.Unlock()
 	var workers []models.Worker
 
 	rows, err := workerrepo.db.Query("SELECT * FROM tasks WHERE userId = ? LIMIT ?", id, limit)
@@ -60,4 +51,17 @@ func (workerrepo *WorkerRepo) GetAll(limit int, id uint64) ([]models.Worker, err
 		return nil, err
 	}
 	return workers, nil
+}
+
+func (workerrepo *WorkerRepo) UpdateExisting(id uint64, record *models.Worker) error {
+	// updating not needed as of now, must add some column to database that makes sense to update
+	return nil
+}
+
+func (workerrepo *WorkerRepo) DeleteByID(ids ...uint64) error {
+	_, err := workerrepo.db.Exec("DELETE FROM tasks WHERE userId = ? AND taskId = ?", ids[0], ids[1])
+	if err != nil {
+		return err
+	}
+	return nil
 }
