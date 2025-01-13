6fcdb45647b997c85eb730b32ee1b839c32aa406
= <=>
2025-01-13 10:08:40 -0500

    refactor: changed the task model to include more columns

--- taskservice/models/task.go
+++ taskservice/models/task.go
@@ -1,10 +1,16 @@
 package models
 
+import "time"
+
 type Task struct {
-	TaskID     uint64 `json:"taskId"` // Primary Key
-	Title      string `json:"title"`
-	Priority   bool   `json:"priority"`
-	CreatorID  uint64 `json:"creatorId"`
-	AssigneeID uint64 `json:"assigneeId"`
-	CreatedAt  string `json:"createdAt"`
+	TaskID      uint64    `json:"taskId"` // Primary Key
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      uint8     `json:"status"`
+	Deadline    time.Time `json:"deadline"`
+	Priority    bool      `json:"priority"`
+	CreatorID   uint64    `json:"creatorId"` // Foreign Key
+	TeamID      uint64    `json:"teamId"`    // Nullable
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
