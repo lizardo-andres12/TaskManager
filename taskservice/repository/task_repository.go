78dfed5ac41bb45b727bbbc9e12722e6a9e116fd
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-01-13 23:51:21 -0500

    refactor: UNSTABLE changing repository functions to work with new schemas

--- taskservice/repository/task_repository.go
+++ taskservice/repository/task_repository.go
@@ -1,5 +1,3 @@
-// --TODO-- refactor all code to match new task schema
-// use query: select * from tasks t join workers w on t.taskId = w.taskId where w.userId=?;
 package repository
 
 import (
@@ -17,18 +15,82 @@ func NewTaskRepo(db *sql.DB) *TaskRepo {
 	return &TaskRepo{DB: db}
 }
 
-func (taskrepo *TaskRepo) CreateNew(ctx context.Context, record *models.Task) error {
+func (taskrepo *TaskRepo) CreateTask(ctx context.Context, task *models.Task) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
 	default:
 	}
 
-	_, err := taskrepo.DB.ExecContext(ctx, "INSERT INTO tasks (Title, Priority, CreatorID, AssigneeID) VALUES (?, ?, ?, ?)", record.Title, record.Priority, record.CreatorID, record.AssigneeID)
+	_, err := taskrepo.DB.ExecContext(
+		ctx,
+		"INSERT INTO task (title, description, deadline, priority, creatorId, teamId) VALUES (?, ?, ?, ?, ?, ?, ?)",
+		task.Title,
+		task.Description,
+		task.Status,
+		task.Deadline,
+		task.Priority,
+		task.CreatorID,
+		task.TeamID,
+	)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (taskrepo *TaskRepo) AssignToTask(ctx context.Context, taskAssignee *models.TaskAssignee) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	_, err := taskrepo.DB.ExecContext(
+		ctx,
+		"INSERT INTO assignee (taskId, assigneeId) VALUES (?, ?)",
+		taskAssignee.TaskID, taskAssignee.AssigneeID,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (taskrepo *TaskRepo) GetAllAssigned(ctx context.Context, id uint64, limit uint32, offset uint32) ([]models.Task, error) {
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	default:
+	}
+
+	var tasks []models.Task
+
+	rows, err := taskrepo.DB.QueryContext(
+		ctx,
+		"SELECT t.title, t.description, t.status, t.deadline, t.priority, t.creatorId, t.teamId FROM task t "+
+			"INNER JOIN assignee a ON t.taskId = a.taskId WHERE a.assigneeId = ? ORDER BY t.createdAt DESC LIMIT ? OFFSET ?",
+		id,
+		limit,
+		offset,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var task models.Task
+		err = rows.Scan(&task.Title, &task.Description, &task.Status, &task.Deadline, &task.Priority, &task.CreatorID, &task.TeamID)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return tasks, nil
 }
 
 func (taskrepo *TaskRepo) GetByTaskID(ctx context.Context, id uint64) (*models.Task, error) {
@@ -40,91 +102,26 @@ func (taskrepo *TaskRepo) GetByTaskID(ctx context.Context, id uint64) (*models.T
 
 	var task models.Task
 
-	row := taskrepo.DB.QueryRowContext(ctx, "SELECT * FROM tasks WHERE taskId = ?", id)
-	if err := row.Scan(&task.TaskID, &task.Title, &task.Priority, &task.CreatorID, &task.AssigneeID, &task.CreatedAt); err != nil {
+	row := taskrepo.DB.QueryRowContext(
+		ctx,
+		"SELECT taskId, title, description, status, deadline, priority, creatorId, teamId FROM task WHERE taskId = ?",
+		id,
+	)
+
+	if err := row.Scan(&task.Title, &task.Description, &task.Status, &task.Deadline, &task.Priority, &task.CreatorID, &task.TeamID); err != nil {
 		return nil, err
 	}
 	return &task, nil
 }
 
-func (taskrepo *TaskRepo) GetAllCreated(ctx context.Context, limit int, id uint64) ([]models.Task, error) {
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	default:
-	}
-
-	var tasks []models.Task
-
-	rows, err := taskrepo.DB.QueryContext(ctx, "SELECT * FROM tasks WHERE creatorId = ? LIMIT ?", id, limit)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var task models.Task
-		if err := rows.Scan(&task.TaskID, &task.Title, &task.Priority, &task.CreatorID, &task.AssigneeID, &task.CreatedAt); err != nil {
-			return nil, err
-		}
-		tasks = append(tasks, task)
-	}
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-	return tasks, nil
-}
-
-func (taskrepo *TaskRepo) GetAllAssigned(ctx context.Context, limit int, id uint64) ([]models.Task, error) {
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	default:
-	}
-
-	var tasks []models.Task
-
-	rows, err := taskrepo.DB.QueryContext(ctx, "SELECT * FROM tasks WHERE assigneeId = ? LIMIT ?", id, limit)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var task models.Task
-		if err := rows.Scan(&task.TaskID, &task.Title, &task.Priority, &task.CreatorID, &task.AssigneeID, &task.CreatedAt); err != nil {
-			return nil, err
-		}
-		tasks = append(tasks, task)
-	}
-	if err = rows.Err(); err != nil {
-		return nil, err
-	}
-	return tasks, nil
-}
-
-func (taskrepo *TaskRepo) UpdateExisting(ctx context.Context, id uint64, record *models.Task) error { // this function should never receive taskId not stored
+func (taskrepo *TaskRepo) UpdateStatus(ctx context.Context, id uint64, status uint8) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
 	default:
 	}
 
-	_, err := taskrepo.DB.ExecContext(ctx, "UPDATE tasks SET title = ?, priority = ?, assigneeId = ? WHERE taskId = ?", record.Title, record.Priority, record.AssigneeID, id)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (taskrepo *TaskRepo) DeleteByTaskID(ctx context.Context, id uint64) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-	}
-
-	_, err := taskrepo.DB.ExecContext(ctx, "DELETE FROM tasks WHERE taskId = ?", id)
+	_, err := taskrepo.DB.ExecContext(ctx, "UPDATE task SET status = ? WHERE taskId = ?", status, id)
 	if err != nil {
 		return err
 	}
