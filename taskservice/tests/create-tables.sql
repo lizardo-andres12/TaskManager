01e8ce7ad344334e1b4c0808d3684b08f3ec16cc
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-01-13 23:52:02 -0500

    update: changed test db script

--- taskservice/tests/create-tables.sql
+++ taskservice/tests/create-tables.sql
@@ -1,20 +1,41 @@
-DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS task, assignee;
 
-CREATE TABLE tasks (
+CREATE TABLE task (
     taskId BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
     title VARCHAR(100) NOT NULL,
+    description VARCHAR(250) NOT NULL,
+    deadline TIMESTAMP NOT NULL,
+    status TINYINT(255) NOT NULL,
     priority TINYINT(1) NOT NULL,
     creatorId BIGINT UNSIGNED NOT NULL,
-    assigneeId BIGINT UNSIGNED DEFAULT 0,
+    teamId BIGINT UNSIGNED,
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`taskId`)
 );
 
-INSERT INTO tasks
-    (title, priority, creatorId, assigneeId)
+CREATE TABLE assignee (
+    id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    taskId BIGINT UNSIGNED NOT NULL,
+    assigneeId BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO task
+    (title, description, deadline, status, priority, creatorId, teamId)
 VALUES
-    ('Pair Program', 0, 1, 3),
-    ('Write Documentation', 1, 2, 3),
-    ('Run Tests', 1, 1, 3),
-    ('Hire Interns', 0, 4, 0),
-    ('Fix Bugs', 0, 5, 0);
+    ('Program', 'Help gavin code', '2025-12-12 00:00:00', 0, 0, 1, NULL),
+    ('Eat', 'cook pasta', '2025-12-11 00:00:00', 1, 1, 2, 4),
+    ('Write some code', 'refactor service layer', '2025-11-12 00:00:00', 0, 1, 1, 2),
+    ('Drive home', 'get all things and go home', '2025-12-12 12:00:00', 0, 2, 1, NULL),
+    ('Run', 'run a mile', '2025-12-12 00:00:30', 0, 1, 1, 2),
+    ('Drink water', 'make sure to drink a gallon of water', '2025-12-1 00:00:00', 2, 0, 1, NULL);
+
+INSERT INTO assignee
+    (taskId, assigneeId)
+VALUES
+    (1, 3),
+    (2, 6),
+    (1, 4),
+    (3, 3),
+    (5, 9);
