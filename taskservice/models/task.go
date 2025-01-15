623596c9c02b9c636de55be68dc52a3d0a0c228f
lizardo-andres12 <lizardo.andres12@gmail.com>
2025-01-14 22:29:18 -0500

    refactor: changed deadline to be string

--- taskservice/models/task.go
+++ taskservice/models/task.go
@@ -1,16 +1,12 @@
 package models
 
-import "time"
-
 type Task struct {
-	TaskID      uint64    `json:"taskId"` // Primary Key
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Status      uint8     `json:"status"`
-	Deadline    time.Time `json:"deadline"`
-	Priority    bool      `json:"priority"`
-	CreatorID   uint64    `json:"creatorId"` // Foreign Key
-	TeamID      uint64    `json:"teamId"`    // Nullable
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
+	TaskID      uint64 `json:"taskId"` // Primary Key
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      uint8  `json:"status"`
+	Deadline    string `json:"deadline"`
+	Priority    bool   `json:"priority"`
+	CreatorID   uint64 `json:"creatorId"` // Foreign Key
+	TeamID      uint64 `json:"teamId"`    // Nullable
 }
